13f11fda4754980b47a2a9bd307d0ffa48c74f62
MichaelFisher1997 <contact@micahelfisher.tech>
2025-05-10 16:33:30 +0100

    Initial flake-based system config

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "NixOS configuration for hypr-nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.hypr-nix = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/hypr-nix/configuration.nix
+      ];
+    };
+  };
+}
+
