bc82db551d736747c3dd5194a1ee4932610e47d8
MichaelFisher1997 <contact@michaelfisher.tech>
2026-06-07 17:46:41 +0100

    fix: use LTS kernel on hyprtop

--- hosts/hyprtop/boot.nix
+++ hosts/hyprtop/boot.nix
@@ -1,7 +1,9 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   imports = [
     ../../modules/core/boot-common.nix
     ../../modules/core/boot-grub.nix
   ];
+
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_12;
 }
